011b20059b5ffa945c938c0be39d5c7aaa543aef
TheEnvixy <nisshakg43@gmail.com>
2025-10-25 11:20:53 -0400

    Basic TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class BasicTeleOp extends OpMode {
+    private DcMotor leftWheel = null;
+    @Override
+    public void init (){
+
+    }
+
+    @Override
+    public void loop (){
+
+    }
+}
