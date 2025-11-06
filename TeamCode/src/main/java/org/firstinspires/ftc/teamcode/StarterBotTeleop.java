c78af12209e30c17ecb772322b14e2daeb8aef38
TheEnvixy <nisshakg43@gmail.com>
2025-11-06 15:22:52 -0500

    Fixing TeleOp to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
@@ -70,7 +70,7 @@ public class StarterBotTeleop extends OpMode {
         indexRight = hardwareMap.get(CRServo.class, "rightServo");
 
         // Incase of wiring into the wrong ports these flags can be switched
-        backSpin.setDirection(DcMotor.Direction.REVERSE);
+        backSpin.setDirection(DcMotor.Direction.FORWARD);
         indexLeft.setDirection(DcMotor.Direction.FORWARD);
         indexRight.setDirection(DcMotor.Direction.REVERSE);
 
